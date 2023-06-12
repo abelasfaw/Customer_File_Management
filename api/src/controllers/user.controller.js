e2bfc1ef70f5a43530aef0dc1ce35b734a40373c
YiacobTilahunKibret <yiacob.tilahun@mmc-edu.net>
2023-06-12 23:12:45 +0300

    all users fetch, single office users fetch, profile update and password change

--- api/src/controllers/user.controller.js
+++ api/src/controllers/user.controller.js
@@ -87,3 +87,133 @@ exports.createUser = async(req, res, next)=>{
     }
 
 }
+exports.fetchAllUsers = async(req, res) => {
+    let paginationSchema = yup.object().shape({
+        page: yup.number().positive(),
+        limit: yup.number().positive()
+    })
+    await paginationSchema.validate(req.query, {abortEarly: false});
+    const numberOfUsers = await User.countDocuments({});
+    let usersQuery = User.find({
+    }, "-password");
+    if (req.query.page && req.query.limit){
+        const skip = (req.query.page - 1) * req.query.limit;
+        if (numberOfUsers > 0 && skip >= numberOfUsers){
+            throw new NotFoundError("page not found")
+        }
+        usersQuery = usersQuery.skip(skip).limit(req.query.limit);
+    }
+    const users = await usersQuery;  
+    return res.json({
+        success: true,
+        totalUsers: numberOfUsers,
+        results: users.length,
+        data: users
+    })  
+}
+//fetchs other users in the same office
+exports.fetchUsersInSingleOffice = async(req, res) => {
+    let paginationSchema = yup.object().shape({
+        page: yup.number().positive(),
+        limit: yup.number().positive()
+    })
+    let parameterSchema = yup.object().shape({
+        office: yup.mixed().oneOf(officeTypes).required()
+    })
+    await paginationSchema.validate(req.query, {abortEarly: false});
+    await parameterSchema.validate(req.params);
+    console.log(`user:${req.user.type}, param: ${req.params.office}`);
+    if (req.user.type !== req.params.office) {
+        console.log("Trying to list users in different office")
+        throw new ValidationError("Cannot fetch users in different office")
+    }
+    //exclude the requesting user to get other users in the same office
+    const numberOfUsers = await User.countDocuments({
+        _id: {$ne: req.user._id},
+        type: req.params.office,
+        status: 'ACTIVE'
+    })
+    let usersQuery = User.find({
+        _id: {$ne: req.user._id},
+        type: req.params.office,
+        status: 'ACTIVE'
+    }, "-password");
+    if (req.query.page && req.query.limit){
+        const skip = (req.query.page - 1) * req.query.limit;
+        if (numberOfUsers > 0 && skip >= numberOfUsers){
+            throw new NotFoundError("page not found")
+        }
+        usersQuery = usersQuery.skip(skip).limit(req.query.limit);
+    }
+    const users = await usersQuery;
+    return res.json({
+        success: true,
+        totalUsers: numberOfUsers,
+        results: users.length,
+        data: users
+    })
+}
+//Update username, firstName and lastname
+exports.updateProfile = async(req, res) => {
+    const bodySchema = yup.object().shape({
+        firstName: yup.string().required(),
+        lastName: yup.string().required(),
+        username: yup.string().required()
+    })
+    const parameterSchema = yup.object().shape({
+        id: yup.string().required()
+    })
+    await bodySchema.validate(req.body);
+    //use user details from authentication middleware
+    let user = req.user
+    if (user.status !== 'ACTIVE') {
+        throw new UnauthorizedError('Access denied')
+    }
+    let duplicateUser = await User.findOne({
+        _id: {$ne: user._id},
+        username: req.body.username,
+    })
+    //check if there is an existing user with the new username
+    if (duplicateUser){
+        console.log(duplicateUser);
+        throw new ValidationError("username taken")
+    }
+    user.firstName = req.body.firstName;
+    user.lastName = req.body.lastName;
+    user.username = req.body.username;
+    await user.save();
+    return res.json({
+        success: true,
+        data: response.removePassword(user)
+    })
+}
+//change user password
+exports.changePassword = async (req, res) => {
+    const bodySchema = yup.object().shape({
+        oldPassword: yup.string().required(),
+        newPassword: yup.string().required(),
+    })
+    // const parameterSchema = yup.object().shape({
+    //     id: yup.string().required()
+    // })
+    // await parameterSchema.validate(req.params);
+    await bodySchema.validate(req.body);
+    let user = req.user;
+    // if (!user) {
+    //     throw new ValidationError("User with specified id not found")
+    // }
+    if (user.status !== 'ACTIVE') {
+        throw new UnauthorizedError('Access denied')
+    }
+    let status = await argon2.verify(user.password, req.body.oldPassword);
+    if (!status){
+        throw new ValidationError("Old Password incorrect")
+    }
+    const newPasswordHash = await argon2.hash(req.body.newPassword);
+    user.password = newPasswordHash;
+    await user.save();
+    return res.json({
+        success: true,
+        data: response.removePassword(user)
+    })
+}
